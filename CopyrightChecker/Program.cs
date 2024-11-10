900f752a6e12da740b4e635ccdfa0e805f0938c2
Ryan Schmidt <rms@rms80.com>
2024-11-09 19:59:06 -0500

    add simple copyright checker

---
+++ CopyrightChecker/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CopyrightChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string CopyrightString = "// Copyright Gradientspace Corp. All Rights Reserved.";
+
+            if (args.Length == 0 )
+            {
+                System.Console.WriteLine("Usage: CopyrightChecker <SourcePath> [-AddIfMissing]");
+                return;
+            }
+
+            // just have to put this in CWD
+            string SourceRootPath = args[0];
+
+            bool bEnableAddIfMissing = args.Contains("-AddIfMissing");
+
+            string[] CsCodeFiles = Directory.GetFiles(SourceRootPath, "*.cs", SearchOption.AllDirectories);
+            string[] CppCodeFiles = Directory.GetFiles(SourceRootPath, "*.cpp", SearchOption.AllDirectories);
+            string[] CppHeaderFiles = Directory.GetFiles(SourceRootPath, "*.h", SearchOption.AllDirectories);
+
+            List<string[]> FileSetsToProcess = new List<string[]>();
+            FileSetsToProcess.Add(CppCodeFiles);
+            FileSetsToProcess.Add(CppHeaderFiles);
+
+            foreach (string[] FileSet in FileSetsToProcess)
+            {
+                foreach (string FilePath in FileSet)
+                {
+                    string[] Lines = File.ReadAllLines(FilePath);
+                    if (Lines[0].StartsWith(CopyrightString) == false)
+                    {
+                        if (bEnableAddIfMissing)
+                        {
+                            List<string> NewLines = new List<string>();
+                            NewLines.Add(CopyrightString);
+                            NewLines.AddRange(Lines);
+                            File.WriteAllLines(FilePath, NewLines);
+                        }
+                        else
+                        {
+                            System.Console.WriteLine(FilePath);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
